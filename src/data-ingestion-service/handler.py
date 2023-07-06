0e853f1c827ee39184bd3fc177da8335e3f6ed64
ciro-greco <ciro.greco@gmail.com>
2023-07-06 11:39:57 -0400

    refactored the configuration yaml, handler.py and added a makefile

--- src/data-ingestion-service/handler.py
+++ src/data-ingestion-service/handler.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
     # Get the S3 bucket name and key from the event
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_key = event['Records'][0]['s3']['object']['key']
-
+    print(bucket_name, file_key)
     # Create a DynamoDB client
     dynamodb = boto3.resource('dynamodb')
     s3 = boto3.client('s3')
